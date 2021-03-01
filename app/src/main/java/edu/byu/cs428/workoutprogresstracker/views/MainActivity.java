bdb7811786942c6e9aa41e2d30ecf252355e0dd7
Jordan Gruenewald <jorg2332@gmail.com>
2021-03-01 15:26:55 -0700

    Created Android project

---
+++ app/src/main/java/edu/byu/cs428/workoutprogresstracker/views/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.byu.cs428.workoutprogresstracker.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.byu.cs428.workoutprogresstracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
