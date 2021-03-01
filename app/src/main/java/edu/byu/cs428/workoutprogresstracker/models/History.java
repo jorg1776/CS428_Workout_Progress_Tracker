21433421c994160576f0a6b5a1bbd5be35aa83c0
weberderek8 <weberderek8@gmail.com>
2021-03-01 16:23:48 -0700

    "Added getters to History.java"

--- app/src/main/java/edu/byu/cs428/workoutprogresstracker/models/History.java
+++ app/src/main/java/edu/byu/cs428/workoutprogresstracker/models/History.java
@@ -8,4 +8,12 @@ import edu.byu.cs428.workoutprogresstracker.models.metric.Metric;
 public class History {
     Metric goal;
     Date timestamp;
+
+    public Metric getGoal() {
+        return goal;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
 }
