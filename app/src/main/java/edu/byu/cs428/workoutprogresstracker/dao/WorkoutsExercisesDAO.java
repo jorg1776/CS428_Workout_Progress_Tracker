52d01f9e0e0f44938b59e6021db538a94cb06d83
Jordan Gruenewald <jorg2332@gmail.com>
2021-03-27 14:38:48 -0600

    Added logic for workout/exercises relationship

--- app/src/main/java/edu/byu/cs428/workoutprogresstracker/dao/WorkoutsExercisesDAO.java
+++ app/src/main/java/edu/byu/cs428/workoutprogresstracker/dao/WorkoutsExercisesDAO.java
@@ -5,7 +5,7 @@ import java.util.List;
 import edu.byu.cs428.workoutprogresstracker.models.Exercise;
 
 public interface WorkoutsExercisesDAO {
-    void addExerciseToWorkout(int exerciseId, int workoutId) throws DataAccessException;
+    void addExerciseToWorkout(Exercise exercise, int workoutId) throws DataAccessException;
     void removeExerciseFromWorkout(int exerciseId, int workoutId) throws DataAccessException;
     List<Exercise> getWorkoutExercises(int workoutId) throws DataAccessException;
 }
